60f898f9d1ef77bc33f40040ae73e24cdac72b2b
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2021-09-10 16:14:51 +0200

    Add layout for views and 404 view
    - Add css files (extract the looper css to 3 files)
    - Add fontawesome webfonts
    - Update composer.json to autoload controllers

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
 
-namespace Looper;
-
-use HomeController;
+namespace Looper\Controllers;
 
 require_once 'vendor/autoload.php';
 
 $action = $_GET['action'] ?? null;
-switch ($action) {
-    case 'home':
-        (new HomeController())->home();
-        break;
-    case 'list-exercices':
-    case 'take-exercise':
-    case 'create-exercise':
-    case 'edit-exercise':
-    case 'manage-exercise':
-    case 'list-takes-exercises':
-    case 'detail-take-exercises':
-    case 'detail-question-exercises':
-        break;
+if ($action) {
+    switch ($action) {
+        case 'home':
+            (new HomeController())->home();
+            break;
+        case 'list-exercices':
+        case 'take-exercise':
+        case 'create-exercise':
+        case 'edit-exercise':
+        case 'manage-exercise':
+        case 'list-takes-exercises':
+        case 'detail-take-exercises':
+        case 'detail-question-exercises':
+            break;
+        default:
+            require_once 'views/pages/404.php';
+            break;
+    }
+} else {
+    (new HomeController())->home();
 }
---
+++ views/includes/head-content.php
@@ -0,0 +1,15 @@
+<!-- Meta Tags -->
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+>
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!-- Milligram -->
+<link rel="stylesheet" href="/views/assets/css/milligram.css">
+
+<!-- Fontawesome -->
+<link rel="stylesheet" href="/views/assets/css/fontawesome.css">
+
+<!-- Fontawesome -->
+<link rel="stylesheet" href="/views/assets/css/styles.css">
